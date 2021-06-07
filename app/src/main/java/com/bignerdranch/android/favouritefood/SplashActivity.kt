0b76f072c399f5f5ed9a849d016c67d2356f2cc9
Djumabaevs <djumabaevb@gmail.com>
2021-06-08 00:39:59 +0600

    add binding

--- app/src/main/java/com/bignerdranch/android/favouritefood/SplashActivity.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/SplashActivity.kt
@@ -2,11 +2,16 @@ package com.bignerdranch.android.favouritefood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.favouritefood.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+      //  splashBinding.tvAppName.text = "KFC"
 
     }
 }
