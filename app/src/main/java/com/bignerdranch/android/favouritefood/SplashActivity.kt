fa56ff698dab5cfdde4fdb5e3e941d90ac94760b
Djumabaevs <djumabaevb@gmail.com>
2021-06-08 00:25:43 +0600

    add new splash

---
+++ app/src/main/java/com/bignerdranch/android/favouritefood/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.favouritefood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
