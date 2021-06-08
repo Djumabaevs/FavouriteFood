92011bb4cc1da4c4a45b1097d6e42a04a5de70f4
Djumabaevs <djumabaevb@gmail.com>
2021-06-08 13:54:22 +0600

    theme bug fixed

--- app/src/main/java/com/bignerdranch/android/favouritefood/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,39 @@ package com.bignerdranch.android.favouritefood.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.bignerdranch.android.favouritefood.R
+import com.bignerdranch.android.favouritefood.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setUpActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+    private fun setUpActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when(v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "Works image!", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
     }
 }
