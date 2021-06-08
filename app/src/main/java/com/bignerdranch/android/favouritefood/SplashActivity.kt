d1c09e12796ae970954a38b70ff7dc08a9a2e562
Djumabaevs <djumabaevb@gmail.com>
2021-06-08 09:24:16 +0600

    hide toolbars

--- app/src/main/java/com/bignerdranch/android/favouritefood/SplashActivity.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.favouritefood
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
 import com.bignerdranch.android.favouritefood.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -10,6 +12,12 @@ class SplashActivity : AppCompatActivity() {
 
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+
+
 
       //  splashBinding.tvAppName.text = "KFC"
 
