f8aac330d42006dd030413ae15220f046e09e3ac
Djumabaevs <djumabaevb@gmail.com>
2021-06-09 10:07:06 +0600

    add dialog custom list

--- app/src/main/java/com/bignerdranch/android/favouritefood/utils/Constants.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/utils/Constants.kt
@@ -39,4 +39,21 @@ object Constants {
 
         return list
     }
+
+    fun dishCookTime(): ArrayList<String> {
+        val list = ArrayList<String>()
+        list.add("10")
+        list.add("15")
+        list.add("20")
+        list.add("30")
+        list.add("45")
+        list.add("50")
+        list.add("60")
+        list.add("90")
+        list.add("120")
+        list.add("150")
+        list.add("180")
+
+        return list
+    }
 }
