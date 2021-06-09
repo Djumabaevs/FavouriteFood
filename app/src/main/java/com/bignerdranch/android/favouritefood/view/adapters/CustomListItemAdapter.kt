e452ea81d4505cc230e363b2ad53af62a2d93421
Djumabaevs <djumabaevb@gmail.com>
2021-06-09 10:56:48 +0600

    use binding object in three implementing methods of adapter class

--- app/src/main/java/com/bignerdranch/android/favouritefood/view/adapters/CustomListItemAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/view/adapters/CustomListItemAdapter.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.favouritefood.view.adapters
 
 import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.favouritefood.databinding.ItemCustomListBinding
 
@@ -13,5 +15,20 @@ class CustomListItemAdapter(
 
     class ViewHolder(view: ItemCustomListBinding) : RecyclerView.ViewHolder(view.root) {
         val tvText = view.tvText
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListBinding =
+            ItemCustomListBinding.inflate(LayoutInflater.from(activity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItems[position]
+        holder.tvText.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
     }
 }
