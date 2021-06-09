ab11057431840e1f45a1dbe66a5ae9b5cbffd653
Djumabaevs <djumabaevb@gmail.com>
2021-06-09 15:05:35 +0600

    ViewModel and ViewModelFactory setup

--- app/src/main/java/com/bignerdranch/android/favouritefood/viewmodel/FavDishViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/viewmodel/FavDishViewModel.kt
@@ -1,15 +1,28 @@
 package com.bignerdranch.android.favouritefood.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.bignerdranch.android.favouritefood.model.database.FavDishRepository
 import com.bignerdranch.android.favouritefood.model.entities.FavDish
 import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
 class FavDishViewModel(private val repository: FavDishRepository) : ViewModel() {
 
     fun insert(dish: FavDish) = viewModelScope.launch {
         repository.insertFavDishData(dish)
+    }
+}
+
+class FavDishViewModelFactory(private val repository: FavDishRepository) :
+        ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(FavDishViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return FavDishViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 
 }
