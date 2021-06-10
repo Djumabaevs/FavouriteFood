ecaa29c3e33ba303888583efa07b360edefa4e87
Djumabaevs <djumabaevb@gmail.com>
2021-06-10 13:23:32 +0600

    bind details to fragment of dish details

--- app/src/main/java/com/bignerdranch/android/favouritefood/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/view/fragments/DishDetailsFragment.kt
@@ -9,10 +9,15 @@ import android.view.ViewGroup
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.favouritefood.R
+import com.bignerdranch.android.favouritefood.databinding.FragmentDishDetailsBinding
+import com.bumptech.glide.Glide
+import java.io.IOException
+import java.util.*
 
 
 class DishDetailsFragment : Fragment() {
 
+    private var mBinding: FragmentDishDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +28,37 @@ class DishDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mBinding = FragmentDishDetailsBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args : DishDetailsFragmentArgs by navArgs()
-        Log.i("Dish title", args.dishDetails.title)
-        Log.i("Dish type", args.dishDetails.type)
+
+        args.let {
+            try {
+                Glide
+                    .with(requireActivity())
+                    .load(it.dishDetails)
+                    .centerCrop()
+                    .into(mBinding!!.ivDishImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            mBinding!!.tvTitle.text = it.dishDetails.title
+            mBinding!!.tvType.text =
+                it.dishDetails.type.capitalize(Locale.ROOT) // Used to make first letter capital
+            mBinding!!.tvCategory.text = it.dishDetails.category
+            mBinding!!.tvIngredients.text = it.dishDetails.ingredients
+            mBinding!!.tvCookingDirection.text = it.dishDetails.directionsToCook
+            mBinding!!.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+        }
+
+     /*   Log.i("Dish title", args.dishDetails.title)
+        Log.i("Dish type", args.dishDetails.type)*/
     }
 }
