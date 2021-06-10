ee29eb67679b5bfaf4f43aa7c61c8deafad8f01d
Djumabaevs <djumabaevb@gmail.com>
2021-06-10 14:03:28 +0600

    palette in the glide listener

--- app/src/main/java/com/bignerdranch/android/favouritefood/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/bignerdranch/android/favouritefood/view/fragments/DishDetailsFragment.kt
@@ -1,16 +1,23 @@
 package com.bignerdranch.android.favouritefood.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bignerdranch.android.favouritefood.R
 import com.bignerdranch.android.favouritefood.databinding.FragmentDishDetailsBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import java.io.IOException
 import java.util.*
 
@@ -41,8 +48,38 @@ class DishDetailsFragment : Fragment() {
             try {
                 Glide
                     .with(requireActivity())
-                    .load(it.dishDetails)
+                    .load(it.dishDetails.image)
                     .centerCrop()
+                    .listener(object: RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("Tag", "Error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate() {
+                                        palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                    mBinding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+
+                    })
                     .into(mBinding!!.ivDishImage)
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -60,5 +97,10 @@ class DishDetailsFragment : Fragment() {
 
      /*   Log.i("Dish title", args.dishDetails.title)
         Log.i("Dish type", args.dishDetails.type)*/
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
